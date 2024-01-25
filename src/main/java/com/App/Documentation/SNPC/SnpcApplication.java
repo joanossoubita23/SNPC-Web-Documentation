1204b7a955c48aed8166e9010c89665248ff2542
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-25 00:02:05 -0500

    first documentation

---
+++ src/main/java/com/App/Documentation/SNPC/SnpcApplication.java
@@ -0,0 +1,13 @@
+package com.App.Documentation.SNPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/App/Documentation/SNPC/SnpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.Documentation.SNPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
