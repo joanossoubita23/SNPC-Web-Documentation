8f8cd4c58b71d47793c3ecc1d85e12c5c2bfdf74
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-25 21:46:27 -0500

    implemtationservice

---
+++ src/main/java/com/App/Documentation/SNPC/service/EmployeeImplementation.java
@@ -0,0 +1,56 @@
+package com.App.Documentation.SNPC.service;
+
+import com.App.Documentation.SNPC.entity.Employee;
+import com.App.Documentation.SNPC.exception.ResourceNotFoundException;
+import com.App.Documentation.SNPC.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+public class EmployeeImplementation implements EmployeeService{
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeImplementation(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getemployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> optionalEmployee=employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()){
+            return optionalEmployee.get();
+        }else {
+            throw new ResourceNotFoundException("Employee with id"+ id +"Is not into the database try again");
+        }
+
+    }
+
+    @Override
+    public Employee AddEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Employee addEmployee(Long id, Employee employee) {
+        Employee existingEmployee=getEmployeeById(id);
+        existingEmployee.setFirstName(existingEmployee.getFirstName());
+        existingEmployee.setLastName(existingEmployee.getLastName());
+        existingEmployee.setEmail(existingEmployee.getEmail());
+        employeeRepository.save(existingEmployee);
+        return existingEmployee;
+    }
+}
--- src/main/java/com/App/Documentation/SNPC/service/EmployeeService.java
+++ src/main/java/com/App/Documentation/SNPC/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.App.Documentation.SNPC.service;
 
+import com.App.Documentation.SNPC.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> getemployee();
+    Employee getEmployeeById(Long id);
+    Employee AddEmployee(Employee employee);
+    void deleteEmployeeById(Long id);
+    Employee addEmployee(Long id, Employee employee);
+
 }
