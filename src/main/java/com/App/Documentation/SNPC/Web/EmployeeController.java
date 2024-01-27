f764145a1f44e803adb521af4d6ca0c62747bd13
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-26 23:58:38 -0500

    userImpel

---
+++ src/main/java/com/App/Documentation/SNPC/Web/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.App.Documentation.SNPC.Web;
+
+public class EmployeeController {
+}
--- src/main/java/com/App/Documentation/SNPC/service/UserServiceImplementation.java
+++ src/main/java/com/App/Documentation/SNPC/service/UserServiceImplementation.java
@@ -1,4 +1,35 @@
 package com.App.Documentation.SNPC.service;
 
-public class UserServiceImplementation {
+import com.App.Documentation.SNPC.entity.User;
+import com.App.Documentation.SNPC.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+
+public class UserServiceImplementation implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        org.springframework.security.core.userdetails.User.UserBuilder builder=null ;
+        Optional<User> user=userRepository.findUserByUsername(username);
+        if (user.isPresent()){
+            User currentUser=user.get();
+            builder=org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(currentUser.getPassword());
+            builder.roles(currentUser.getRole());
+        }else {
+            throw new UsernameNotFoundException("User Not found");
+        }
+        return builder.build();
+    }
 }
