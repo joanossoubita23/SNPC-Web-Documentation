043ce9110b9340da210f74354f25c800add31acb
joanossoubita23 <joanossoubita@yahoo.fr>
2024-02-10 22:46:34 -0500

    sec

--- src/main/java/com/App/Documentation/SNPC/security/AuthenticationFilter.java
+++ src/main/java/com/App/Documentation/SNPC/security/AuthenticationFilter.java
@@ -1,4 +1,26 @@
 package com.App.Documentation.SNPC.security;
 
-public class AuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwsToken=new request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (jwsToken !=null){
+            String user=jwtService.getAuthUser(request)
+
+        }
+    }
 }
--- src/main/java/com/App/Documentation/SNPC/security/SecurityConfig.java
+++ src/main/java/com/App/Documentation/SNPC/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.App.Documentation.SNPC.security;
 
+import com.App.Documentation.SNPC.exception.AUthEntrypoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 public class SecurityConfig {
+    @Autowired
+    private AUthEntrypoint aUthEntrypoint;
+    @Autowired
+    private AuthenticationFilter authenticationFilter;
+    @Autowired
+    private  final UserDetailsService userDetailsService;
+
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+}
 }
