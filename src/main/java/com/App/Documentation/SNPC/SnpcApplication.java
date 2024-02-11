da7c5fe67f4ecafb777da88e60228f567f5a3f7e
joanossoubita23 <joanossoubita@yahoo.fr>
2024-02-10 22:22:43 -0500

    app

--- src/main/java/com/App/Documentation/SNPC/SnpcApplication.java
+++ src/main/java/com/App/Documentation/SNPC/SnpcApplication.java
@@ -1,20 +1,38 @@
 package com.App.Documentation.SNPC;
 
+import com.App.Documentation.SNPC.repository.DocumentRepository;
+import com.App.Documentation.SNPC.repository.EmployeeRepository;
+import com.App.Documentation.SNPC.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.logging.java.JavaLoggingSystem;
 
 @SpringBootApplication
 public class SnpcApplication implements CommandLineRunner {
+	@Autowired
+	 private DocumentRepository documentRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private UserRepository userRepository;
+
+	private  static  final Logger logger= LoggerFactory.getLogger(SnpcApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnpcApplication.class, args);
+		logger.info("Application Start");
 
 
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+
 
 	}
 }
